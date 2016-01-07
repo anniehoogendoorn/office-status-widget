962b0bf58a6c26d90b5a2c12c073ebc296574159
Annie <anniehoogendoorn@gmail.com>
2016-01-06 18:07:34 -0800

    Add widget.js.

--- js/widget.js
+++ js/widget.js
@@ -7,9 +7,20 @@ xhr.onreadystatechange = function () {
       //typeof operator looks at the item that is listed next and returns a word to describe the javascript element
       // console.log(typeof xhr.responseText);
 
-      //Parses the ajax response into a javascript object  
+      //Parses the ajax response into a javascript object
       var employees = JSON.parse(xhr.responseText);
-
+      var statusHTML = '<ul class="bulleted">';
+      for (var i=0; i<employees.length; i+= 1) {
+        if (employees[i].inoffice === true) {
+          statusHTML += '<li class="in">';
+        } else {
+          statusHTML += '<li class="out">';
+        }
+        statusHTML += employees[i].name;
+        statusHTML += '</li>';
+      }
+      statusHTML += '</ul>';
+      document.getElementById('employeeList').innerHTML = statusHTML;
     }
 };
 xhr.open('GET', 'data/employees.json');
